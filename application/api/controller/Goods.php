1412a7d501c1f52711d65973235e920856cbe466
Godisfooler <1007005246@qq.com>
2023-04-24 02:21:53 +0800

    tijiao

--- application/api/controller/Goods.php
+++ application/api/controller/Goods.php
@@ -14,6 +14,11 @@ class Goods extends Controller
 
     public function getCategory(){
         $list = db('categorys')->select();
+        foreach($list as &$l){
+            if(strpos($l['sImgUrl'],'http') === false){
+                $l['sImgUrl'] = 'http://'.$_SERVER['SERVER_NAME'].ltrim($l['sImgUrl'],'.');
+            }
+        }
         $return = list_to_tree($list,0,'id','parentid','children');
 
         $this->result($return,10000,'请求成功','json');
@@ -22,6 +27,9 @@ class Goods extends Controller
     public function getGoodslist(){
         $categoryid = input('categoryid');
         $list = db('goods_list')->where(['pid'=>$categoryid])->select();
+        foreach($list as &$l){
+            $l['sImgUrls'] = 'http://'.$_SERVER['SERVER_NAME'].$l['sImgUrls'];
+        }
         $this->result($list,10000,'请求成功','json');
     }
 
@@ -29,6 +37,82 @@ class Goods extends Controller
     public function publishlist(){
         $uid = empty(input('uid'))?1:input('uid');
         $list = db('goods_list')->where(['uid'=>$uid])->select();
+        foreach($list as &$l){
+            if(strpos($l['sImgUrls'],'http') === false){
+                $l['sImgUrls'] = 'http://'.$_SERVER['SERVER_NAME'].ltrim($l['sImgUrls'],'.');
+            }
+        }
+        $temp = [];
+        $temp['status'] = 1;
+        $temp['statusDesc'] = '已成交';
+        $temp['goodsList'] = $list;
+        $reutrn[$temp['status']] = $temp;
+        $temp['statusDesc'] = '待成交';
+        $reutrn[2] = $temp;
+        $this->result($reutrn,10000,'请求成功','json');
+    }
+
+    public function goodspublish(){
+        $imgArr = json_decode(input('imgs'),true);
+        $data = [];
+        $data['sGoodsName'] = input('goodsname');
+        $data['sPrice'] = input('price');
+        $data['sDescribe'] = input('descripe');
+        $data['pid'] = input('categoryid');
+        $data['uid'] = input('uid');
+        foreach($imgArr as $img){
+            $imgUrl = str_replace('http://','http://127.0.0.1:43374/',$img['path']);
+            $imgPath = $this->downloadImage($imgUrl);
+            $data['sImgUrls'] = '/images/goods/'.$imgPath;
+            $imgPathArr[] = '/images/goods/'.$imgPath;
+        }
+        $data['sImgList'] = json_encode($imgPathArr);
+        $data['iAddTime'] = time();
+        $res = db('goods_list')->insert($data);
+        if($res){
+            $this->result($res,10000,'请求成功','json');
+        }
+    }
+
+    public function uploadFile(){
+        $file = request()->file('image');
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'avatarUrl');
+            if($info){
+                // 成功上传后 获取上传信息
+                $filePath = $info->getSaveName(); 
+            }else{
+            }
+        }
+        $filePath = './avatarUrl/'.str_replace('\\','/',$filePath);
+        return $filePath;
+    }
+
+    public function downloadImage($url)
+    {
+        // 获取图片数据
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        // 创建文件夹并保存图片
+        $imageFolder = ROOT_PATH.'public'. DS .'/images/goods/';
+        if (!is_dir($imageFolder)) {
+            mkdir($imageFolder);
+        }
+        $imageName = uniqid() . '.jpg';
+        $imagePath = $imageFolder . $imageName;
+
+        $file = fopen($imagePath, 'w');
+        fwrite($file, $data);
+        fclose($file);
+        return $imageName;
+    }
+
+    public function getCategorylist(){
+        $list = db('categorys')->where(['parentid'=>['GT',0]])->select();
         $this->result($list,10000,'请求成功','json');
     }
 }
--- application/api/controller/Login.php
+++ application/api/controller/Login.php
@@ -16,33 +16,50 @@ class Login extends Controller
         $res = db('user_list')->where($map)->find();
         if($res){
             header('uid:'.$res['id']);
+            header('username:'.$res['username']);
             $this->result([],10000,'登录成功','json');
         }else{
-            $this->result($res,10000,'账号或密码错误','json');
+            $this->result($res,10001,'账号或密码错误','json');
         }
     } 
 
     public function regist(){
+        $imgUrl = $this->uploadFile();
         $username = input('username');
         $password = input('password');
         $data = [];
         $data['username'] = $username;
         $data['password'] = md5($password);
+        $data['avatarUrl'] = $imgUrl;
+        $data['iAddTime'] = time();
         $res = db('user_list')->insert($data);
         if($res){
             $this->result($res,10000,'注册成功','json');
         }else{
-            $this->result($res,10000,'注册失败','json');
+            $this->result($res,10001,'注册失败','json');
         }
     }
 
-    public function getCategory(){
-        $uid = $_COOKIE['uid'];
-        var_dump($uid);exit;
-        $list = db('categorys')->select();
-        $return = list_to_tree($list,0,'id','parentid','children');
+    public function uploadFile(){
+        $file = request()->file('image');
+        if($file){
+            $info = $file->move(ROOT_PATH . 'public' . DS . 'avatarUrl');
+            if($info){
+                // 成功上传后 获取上传信息
+                $filePath = $info->getSaveName(); 
+            }else{
+            }
+        }
+        $filePath = './avatarUrl/'.str_replace('\\','/',$filePath);
+        return $filePath;
+    }
 
-        $this->result($return,10000,'请求成功','json');
+    public function getUserImg(){
+        $uid = input('uid');
+        $list = db('user_list')->field('avatarUrl')->find($uid);
+        $list['avatarUrl'] = 'http://'.$_SERVER['SERVER_NAME'].ltrim($list['avatarUrl'],'.');
+
+        $this->result($list,10000,'请求成功','json');
     }
 
     public function getGoodslist(){
