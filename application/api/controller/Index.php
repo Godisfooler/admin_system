db9d645c520b2448e341bef10582a979136f9075
Godisfooler <1007005246@qq.com>
2023-05-11 02:10:49 +0800

    添加字段方法

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -1,10 +1,19 @@
 <?php
 namespace app\api\controller;
+use think\Db;
 
 class Index
 {
     public function index()
     {
         return 'index';
+    }
+
+    public function addField(){
+        $sql = "ALTER TABLE `wechat_news_applet`.`user_list` 
+        ADD COLUMN `nickname` varchar(255) NOT NULL DEFAULT '' COMMENT '昵称' AFTER `username`,
+        ADD COLUMN `gender` varchar(255) NOT NULL DEFAULT '' COMMENT '性别' AFTER `nickname`";
+        $list = Db::query($sql);
+        echo('执行成功！');
     }
 }
